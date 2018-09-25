f1a86b3deeb898780a46546c9662dd5a5422e2d7
xiaoming <1406629722@qq.com>
2018-09-25 18:47:08 +0800

    keep fixing

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-wget https://files.pythonhosted.org/packages/b4/8d/8dbb8b6745d7a59084cf1b28837b32c9717c1b4a97333d5b25e25fa9813b/sphinxcontrib-httpdomain-1.7.0.tar.gz
-pip install sphinxcontrib-httpdomain-1.7.0.tar.gz
-wget https://files.pythonhosted.org/packages/a3/69/96b14fbf64d94e7f8cb0ca2d891827068c139d73081d220af4684c33d76e/sphinxcontrib-httpexample-0.9.0.tar.gz
-pip install sphinxcontrib-httpexample-0.9.0.tar.gz
+easy_install -U pip
+#python -m pip install --upgrade pip
+pip install sphinxcontrib-httpdomain
+pip install sphinxcontrib-httpexample
