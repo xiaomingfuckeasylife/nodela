27cbf4008df68dd2997811966300b6e554860567
xiaoming <1406629722@qq.com>
2018-09-26 13:17:39 +0800

    fixing

--- docs/conf.py
+++ docs/conf.py
@@ -276,7 +276,9 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    
+    ('index', 'sphinxcontrib-httpdomain.tex',
+   u'sphinxcontrib-httpdomain Documentation',
+   u'Hong Minhee', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
