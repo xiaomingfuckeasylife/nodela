1bd3ad89e0dd33353ed575e047e312adf53aee05
xiaoming <1406629722@qq.com>
2018-09-26 13:49:04 +0800

    fixing

--- docs/conf.py
+++ docs/conf.py
@@ -280,6 +280,8 @@ latex_documents = [
     ('index', 'sphinxcontrib-httpdomain.tex',
    u'sphinxcontrib-httpdomain Documentation',
    u'Hong Minhee', 'manual'),
+    (master_doc, 'sphinxcontrib-httpexample.tex', u'sphinxcontrib-httpexample Documentation',
+     u'Asko Soukka', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
